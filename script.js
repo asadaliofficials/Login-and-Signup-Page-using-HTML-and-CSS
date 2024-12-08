47a16b80acea1268fd097881c5282a2805afbc92
Asad Ali <134801060+asadaliofficials@users.noreply.github.com>
2024-12-08 20:53:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
 var buttons = document.getElementsByTagName("button");
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(event) {
+    buttons[i].addEventListener("click", async function (event) {
+
+        document.getElementById('heading').classList.remove("h1Change");
+        document.getElementById('under-line').classList.remove("h1Change");
+        void document.getElementById('heading').offsetWidth;
+        void document.getElementById('under-line').offsetWidth;
+        document.getElementById('heading').classList.add("h1Change");
+        document.getElementById('under-line').classList.add("h1Change");
+
+
+
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.add("disabled");
+        };
+        this.classList.remove("disabled");
+
 
 
         // Check which button was clicked
         if (this.textContent.trim() === "Sign Up") {
             // Code for "Sign Up" button
             document.getElementById('heading').innerText = "Sign Up";
-    
-            document.getElementById('name').style.display = 'block';
+
+            // document.getElementById('name').style.maxHeight = '44px';
+            document.getElementById('name').classList.remove('hidden'); // To show again
+
             document.getElementById('para').innerHTML = "Password Suggestions <span>Click Here!</span>";
             document.getElementById('para').style.transform = 'translateX(-30px)';
 
-            // Styling for buttons
-            const secondButton = document.querySelector('#buttons > :nth-child(2)');
-            secondButton.style.backgroundColor = '#e8e8e8';
-            secondButton.style.color = '#868686';
-            const firstButton = document.querySelector('#buttons > :nth-child(1)');
-            firstButton.style.backgroundColor = 'green';
-            firstButton.style.color = 'white';
-
-            const buttons = document.querySelector('#buttons');
-            buttons.style.marginTop = '66px';
-
-            // Reset form height and transition class
-            document.getElementById('form').style.maxHeight = '362px';
 
 
         } else {
             // Code for "Sign In" button
             document.getElementById('heading').innerText = "Sign In";
-            document.getElementById('name').style.display = 'none';
+            document.getElementById('name').classList.add('hidden'); // To hide
+
             document.getElementById('para').innerHTML = "Forget Password <span>Click Here</span>";
             document.getElementById('para').style.transform = 'translateX(-50px)';
-
-            // Styling for buttons
-            const firstButton = document.querySelector('#buttons > :nth-child(1)');
-            firstButton.style.backgroundColor = '#e8e8e8';
-            firstButton.style.color = '#868686';
-            const secondButton = document.querySelector('#buttons > :nth-child(2)');
-            secondButton.style.backgroundColor = 'green';
-            secondButton.style.color = 'white';
-
-            const buttons = document.querySelector('#buttons');
-            buttons.style.marginTop = '145px';
-
-
         }
     });
 }
